d7b2b7756cce6e5e6e535f1652737c58b559a836
Ali <ali0207k@gmail.com>
2022-08-02 21:58:58 +0300

    Created

---
+++ app.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const express = require('express');
+const vhost = require('vhost');
+
+const app = express();
+const main = express();
+const api = express();
+const port = process.env.PORT || 80;
+
+main.get('/', (req, res) => {
+    res.sendFile(__dirname + '/pages/top.html');
+});
+api.get('/', (req, res) => {
+    res.send('Welcome to api!');
+});
+
+app.use(vhost('alinco8.com', main));
+app.use(vhost('www.alinco8.com', main));
+app.use(vhost('api.alinco8.com', main));
+
+app.listen(port, () => {
+    console.log('Server listen on port: %d', port);
+});
