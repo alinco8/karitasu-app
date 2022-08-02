296d9933a83e8c51af47be8575aa0badbcb7a1ff
Ali <ali0207k@gmail.com>
2022-08-02 22:09:47 +0300

    delete vhost

--- app.js
+++ app.js
@@ -2,20 +2,11 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
-const main = express();
-const api = express();
 const port = 3000;
 
-main.get('/', (req, res) => {
-    res.sendFile(__dirname + '/pages/top.html');
-});
-api.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Welcome to api!');
 });
-
-app.use(vhost('alinco8.com', main));
-app.use(vhost('www.alinco8.com', main));
-app.use(vhost('api.alinco8.com', main));
 
 app.listen(port, () => {
     console.log('Server listen on port: %d', port);
